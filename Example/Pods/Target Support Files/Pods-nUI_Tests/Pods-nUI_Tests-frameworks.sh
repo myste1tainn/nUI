7d9558a3ff1418f1b720f2f60f9e76c806b16efd
A. Keereena <arnon@invp.co.th>
2016-03-07 13:17:38 +0700

    Update example

--- Example/Pods/Target Support Files/Pods-nUI_Tests/Pods-nUI_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-nUI_Tests/Pods-nUI_Tests-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-nUI_Tests/nUI.framework"
-  install_framework "Pods-nUI_Tests/FBSnapshotTestCase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-nUI_Tests/nUI.framework"
-  install_framework "Pods-nUI_Tests/FBSnapshotTestCase.framework"
 fi
